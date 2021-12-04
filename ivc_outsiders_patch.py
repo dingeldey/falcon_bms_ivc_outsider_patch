69250c1df812e2322524ec2208e10abfafeb9cba
dingeldey <94619323+dingeldey@users.noreply.github.com>
2021-12-04 12:06:36 +0100

    added submodule

--- ivc_outsiders_patch.py
+++ ivc_outsiders_patch.py
@@ -3,10 +3,31 @@
 """
 import os.path
 import traceback
+import re
+
+
+def read_file_lines(file_path):
+    with open(file_path) as f:
+        return f.readlines()
+
+def replace_outsider(lines):
+
 
 
 def main():
-    pass
+    cwd_path = os.getcwd()
+    client_ini_path = os.path.join(cwd_path, "Bin/x64/IVC/IVC Client.ini")
+    client_ini_path_bkp = os.path.join(cwd_path, "Bin/x64/IVC/IVC Client_backup.ini")
+
+    make_ini_backup(client_ini_path, client_ini_path_bkp)
+    ini_lines = read_file_lines(client_ini_path)
+    replace_outsider(ini_lines)
+
+
+
+def make_ini_backup(client_ini_path, client_ini_path_bkp):
+    from shutil import copyfile
+    copyfile(client_ini_path, client_ini_path_bkp)
 
 
 if __name__ == "__main__":
